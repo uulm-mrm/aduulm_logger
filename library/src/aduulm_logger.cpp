14625bf8fe0dbd7b84c4a3c45010b2df2036a1cb
Martin Herrmann <martin.herrmann@uni-ulm.de>
2018-09-27 09:45:45 +0200

    [UPDATE] Transfer catkin dependent stuff to ros CMakeLists.txt

--- library/src/aduulm_logger.cpp
+++ library/src/aduulm_logger.cpp
@@ -3,4 +3,4 @@
 std::ofstream g_oFile;
 std::string g_file_name;
 uint16_t g_log_level = 2;
-std::string aduulm_logger_VERSION_ext = aduulm_logger_VERSION;
+std::string aduulm_logger_VERSION_ext = aduulm_logger_v;
