57238e08f388f256e750be03c10aa1031f0c0a37
Jan Strohbeck <jan.strohbeck@uni-ulm.de>
2023-03-14 13:40:27 +0000

    [UPDATE] Drop support for ROS1 in python logger, adjust interface to be similar to C++ logger

--- library/python/aduulm_logger_python/logger.py
+++ library/python/aduulm_logger_python/logger.py
@@ -14,85 +14,109 @@ class LoggerWrapper:
 
 
 class Logger:
-    _levels = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+    _levels = ["None", "Error", "Warn", "Info", "Debug"]
+    _levels_ints = list(range(len(_levels)))
 
     def __init__(self, *args, **kwargs):
+        self.level = 'Info'
+
         def use_print_logger():
             self.functions = {
-                "DEBUG": lambda *args, **kwargs: self._do_print("DEBUG", *args),
-                "INFO": lambda *args, **kwargs: self._do_print("INFO", *args),
-                "WARNING": lambda *args, **kwargs: self._do_print("WARNING", *args),
-                "ERROR": lambda *args, **kwargs: self._do_print("ERROR", *args),
-                "CRITICAL": lambda *args, **kwargs: self._do_print("CRITICAL", *args),
+                "Debug": lambda *args, **kwargs: self._do_print("Debug", *args),
+                "Info": lambda *args, **kwargs: self._do_print("Info", *args),
+                "Warn": lambda *args, **kwargs: self._do_print("Warn", *args),
+                "Error": lambda *args, **kwargs: self._do_print("Error", *args),
                 "SET_LEVEL": lambda level: None,
             }
+
+        # For debug purposes:
         # use_print_logger()
-        # self.set_level("INFO")
+        # self.set_level("Info")
         # return
+
         try:
-            import rospy
-            self.functions = {
-                "DEBUG": rospy.logdebug,
-                "INFO": rospy.loginfo,
-                "WARNING": rospy.logwarn,
-                "ERROR": rospy.logerr,
-                "CRITICAL": rospy.logfatal,
-                "SET_LEVEL": lambda level: logging.getLogger('rosout').setLevel(getattr(logging, level)),
+            import rclpy
+            import rclpy.logging
+            from rclpy.impl.rcutils_logger import _internal_callers
+            import os.path
+            _internal_callers.append(os.path.realpath(__file__),)
+            rclpy.logging
+            from rclpy.logging import LoggingSeverity
+            node = args[0]
+            args = args[1:]
+            logger_levels_to_severity = {
+                "Debug": LoggingSeverity.DEBUG,
+                "Info": LoggingSeverity.INFO,
+                "Warn": LoggingSeverity.WARN,
+                "Error": LoggingSeverity.ERROR,
+                "None": LoggingSeverity.FATAL,
             }
+            self.functions = {
+                "Debug": node.get_logger().debug,
+                "Info": node.get_logger().info,
+                "Warn": node.get_logger().warning,
+                "Error": node.get_logger().error,
+                "SET_LEVEL": lambda level: rclpy.logging.set_logger_level(node.get_logger().name, logger_levels_to_severity[level]),
+            }
+        except IndexError:
+            # Maybe the logger will be initialized later, do not complain for now...
+            use_print_logger()
         except ImportError:
-            try:
-                import rclpy
-                import rclpy.logging
-                from rclpy.impl.rcutils_logger import _internal_callers
-                import os.path
-                _internal_callers.append(os.path.realpath(__file__),)
-                rclpy.logging
-                from rclpy.logging import LoggingSeverity
-                node = args[0]
-                args = args[1:]
-                self.functions = {
-                    "DEBUG": node.get_logger().debug,
-                    "INFO": node.get_logger().info,
-                    "WARNING": node.get_logger().warning,
-                    "ERROR": node.get_logger().error,
-                    "CRITICAL": node.get_logger().fatal,
-                    "SET_LEVEL": lambda level: rclpy.logging.set_logger_level(node.get_logger().name, getattr(LoggingSeverity, level)),
-                }
-            except IndexError:
-                # Maybe the logger will be initialized later, do not complain for now...
-                use_print_logger()
-            except ImportError:
-                print("No logging backend found, falling back to print().")
-                use_print_logger()
-        self.set_level("INFO")
+            print("No logging backend found, falling back to print().")
+            use_print_logger()
+
+        self.set_level("Info")
 
     def set_level(self, level):
+        is_int = isinstance(level, int)
+        is_str = isinstance(level, str)
+        if is_int:
+            try:
+                level = self._levels[level]
+            except IndexError:
+                self.error(f"Not a valid logger level number: {level}")
+                return
+        elif (is_str and level not in self._levels) or not is_str:
+            self.error(f"Not a valid logger level: {level}")
+            return
         self.level = level
         self.functions["SET_LEVEL"](level)
 
+    def get_level(self):
+        return self.level
+
     def _do_print(self, level, *args):
-        if self._levels.index(level) < self._levels.index(self.level):
+        if self._levels.index(level) > self._levels.index(self.level):
             return
-        print(f"[{level}]", *args)
+        print(f"[{level.upper()}]", *args)
 
     def _print(self, level, args):
         s = " ".join(str(a) for a in args)
         self.functions[level](s)
 
     def debug(self, *args):
-        self._print("DEBUG", args)
+        self._print("Debug", args)
+
+    def deb(self, *args):
+        self._print("Debug", args)
 
     def info(self, *args):
-        self._print("INFO", args)
+        self._print("Info", args)
+
+    def inf(self, *args):
+        self._print("Info", args)
 
     def warning(self, *args):
-        self._print("WARNING", args)
+        self._print("Warn", args)
+
+    def warn(self, *args):
+        self._print("Warn", args)
 
     def error(self, *args):
-        self._print("ERROR", args)
+        self._print("Error", args)
 
-    def critical(self, *args):
-        self._print("CRITICAL", args)
+    def err(self, *args):
+        self._print("Error", args)
 
 
 logger = None
